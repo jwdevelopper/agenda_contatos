af86d8421705562c580425f79fa0c1e16d53a6f5
jwdevelopper <58191292+jwdevelopper@users.noreply.github.com>
2021-02-24 15:04:32 -0300

    Inicio do rep

---
+++ lib/model/Contato.dart
@@ -0,0 +1,39 @@
+final String idColumn = "idColumn";
+final String nomeColumn = "nomeColumn";
+final String emailColumn = "emailColumn";
+final String telefoneColumn = "telefoneColumn";
+final String imgColumn = "imgColumn";
+
+class Contato {
+  int id;
+  String nome;
+  String email;
+  String telefone;
+  String img;
+
+  Contato.fromMap(Map map) {
+    id = map[idColumn];
+    nome = map[nomeColumn];
+    email = map[emailColumn];
+    telefone = map[telefoneColumn];
+    img = map[imgColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nomeColumn: nome,
+      emailColumn: email,
+      telefoneColumn: telefone,
+      imgColumn: img
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contato(id: $id, nome: $nome, email: $email, telefone: $telefone, img: $img)";
+  }
+}
