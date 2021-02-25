de7003128a326e59bd723abe48ad931044fb3360
jwdevelopper <58191292+jwdevelopper@users.noreply.github.com>
2021-02-25 14:02:00 -0300

    inicio da configuracao do DB

--- lib/helpers/contato_helper.dart
+++ lib/helpers/contato_helper.dart
@@ -1,5 +1,28 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class ContatoHelper {
+  static final ContatoHelper _instance = ContatoHelper.internal();
 
+  factory ContatoHelper() => _instance;
+
+  ContatoHelper.internal();
+
+  Database _db;
+
+  get db {
+    if(_db != null) {
+      return _db; 
+    } else {
+      _db = initDb();
+    }
+  }
+
+  initDb() async{
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contatos.db");
+    openDatabase(path, version: 1, onCreate: (Database db, int novaVersao) async {
+      await db.execute();
+    });
+  }
 }
